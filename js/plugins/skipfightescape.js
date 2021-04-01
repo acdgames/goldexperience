94415ece3798cf212f0fea1aaaaf40da9c9d04d4
acdgames <acdgames@gmail.com>
2021-04-01 14:42:08 -0700

    Elysium Update
    Changed project to test Elysium

--- js/plugins/skipfightescape.js
+++
@@ -1,8 +0,0 @@
-var SceneBattlePartyCommand = Scene_Battle.prototype.startPartyCommandSelection;
-Scene_Battle.prototype.startPartyCommandSelection = function() {
-    SceneBattlePartyCommand.call(this);
-    this.selectNextCommand();
-    this._helpWindow.clear();
-    this._partyCommandWindow.deactivate();
-};
-
